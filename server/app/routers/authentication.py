f680390f6b8722ba5ad995961bf0a617ca648688
oPECHo <opecho0280@gmail.com>
2023-04-07 11:29:06 +0700

    create router login token

---
+++ server/app/routers/authentication.py
@@ -0,0 +1,25 @@
+from fastapi import APIRouter, Depends, status, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
+
+from ..core import database, models
+from ..utils import token
+from sqlalchemy.orm import Session
+from ..utils.hashing import Hash
+
+router = APIRouter(
+    tags=['Login']
+)
+
+@router.post('/login')
+async def login(request:OAuth2PasswordRequestForm = Depends(), db: Session = Depends(database.get_db)):
+    user = db.query(models.User).filter(models.User.email == request.username).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f'Invalid Credentials')
+
+    if not Hash.verify(user.password, request.password):
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f'Incorrect password')
+
+    access_token = token.create_access_token(data={"sub": user.email})
+    return {"access_token": access_token, "token_type": "bearer"}
