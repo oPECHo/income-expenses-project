ddafeac3578bf4df1ff87737316bfe1890730621
oPECHo <opecho0280@gmail.com>
2023-04-07 11:23:04 +0700

    add router in main

--- server/app/main.py
+++ server/app/main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
 from .core import models
 from .core.database import engine
-from .routers import transaction
+from .routers import transaction, users
 
 app = FastAPI()
 
 models.Base.metadata.create_all(engine)
 
+app.include_router(users.router)
 app.include_router(transaction.router)
