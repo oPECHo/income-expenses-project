0b55e8f004378817add9fae0d4b84a035d0a3327
oPECHo <opecho0280@gmail.com>
2023-04-07 11:30:29 +0700

    add router authentication in main

--- server/app/main.py
+++ server/app/main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
 from .core import models
 from .core.database import engine
-from .routers import transaction, users
+from .routers import transaction, users, authentication
 
 app = FastAPI()
 
 models.Base.metadata.create_all(engine)
 
+app.include_router(authentication.router)
 app.include_router(users.router)
 app.include_router(transaction.router)
