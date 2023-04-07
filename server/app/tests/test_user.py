2fbd9f6330053d4137558d54c944ef1466726f84
oPECHo <opecho0280@gmail.com>
2023-04-07 13:52:00 +0700

    create test_user method post register

---
+++ server/app/tests/test_user.py
@@ -0,0 +1,19 @@
+import sys
+sys.path.insert(0, "/home/nattanon/project/income-expenses-project/server")
+
+from fastapi.testclient import TestClient
+from app.main import app
+
+client = TestClient(app)
+
+def test_create_user():
+    user = {
+        "username": "tester",
+        "email": "tester@gmail.com",
+        "password": "password test",
+    }
+    response = client.post("/register", json=user)
+    assert response.status_code == 200
+    assert response.json()["username"] == user["username"]
+    assert response.json()["email"] == user["email"]
+
