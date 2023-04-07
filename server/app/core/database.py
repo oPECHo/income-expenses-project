a03289fb3a50f7ba4f6cd852b997bb6061eb5e74
oPECHo <opecho0280@gmail.com>
2023-04-07 13:29:10 +0700

    edit path database

--- server/app/core/database.py
+++ server/app/core/database.py
@@ -1,11 +1,10 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-SQLALCHAMY_DATABASE_URL = 'sqlite:///./app/database.db'
+SQLALCHEMY_DATABASE_URL = 'sqlite:///./database.db'
 
-engine = create_engine(SQLALCHAMY_DATABASE_URL,connect_args={"check_same_thread": False})
-SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
